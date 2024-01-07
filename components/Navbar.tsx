b86adeeb3e932af94d67222941c566931f12d1da
dinogomez <dinogomez117@gmail.com>
2024-01-07 21:04:21 +0800

    Navbar Fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -9,20 +9,21 @@ export default function Navbar() {
   const pathname = usePathname();
   const isHomePage = pathname === "/";
   return (
-    <nav className="flex flex-col  items-center px-12 py-12 sm:px-24 md:px-24 lg:px-16">
+    <nav className="flex flex-col  items-center px-12 py-12 sm:px-24 md:px-24   ">
       <div className="flex flex-col sm:flex-row w-full justify-between ">
-        <Link
-          href="/"
-          className={` ${
-            !isHomePage
-              ? "hover:decoration-green-500 hover:decoration-wavy hover:underline underline decoration-slice decoration-blue-500"
-              : ""
-          }`}
-        >
+        <Link href="/">
           <div className="flex space-x-3">
             <div className="flex flex-col">
               <div className="relative">
-                <h1 className="text-3xl tracking-wider">openimage</h1>
+                <h1
+                  className={`text-3xl tracking-wider ${
+                    !isHomePage
+                      ? "hover:decoration-green-500 hover:decoration-wavy hover:underline underline decoration-slice decoration-blue-500"
+                      : ""
+                  }`}
+                >
+                  openimage
+                </h1>
                 <div className="flex  md:absolute  -top-0 -right-40 space-x-1  font-mono  text-sm ">
                   <h1 className="text-white bg-green-500 px-1">ad-free</h1>
                   <h1 className="text-white bg-blue-500 px-1">no-upload</h1>
