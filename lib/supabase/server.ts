6bffd94a03d795c9100554b92feff634b276e59e
dinogomez <dinogomez117@gmail.com>
2024-01-07 14:31:54 +0800

    FIX: ENV

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,10 +1,9 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''; // Provide a default value or handle it according to your use case
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''; // Provide a default value or handle it according to your use case
+
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '');
 
 export default supabase;
