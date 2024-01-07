13b6bf350fb4d453369c01956447507b1b15a28f
dinogomez <dinogomez117@gmail.com>
2024-01-07 14:34:44 +0800

    Fix: Server.TS

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,7 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 
 
 
-// Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL || '', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '');
 
 export default supabase;
